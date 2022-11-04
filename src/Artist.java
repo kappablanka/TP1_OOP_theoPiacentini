3ac8557427e570c22cf862a3662b67f16654c061
kappablanka <theo.piacentini@gmail.com>
2022-11-04 15:49:36 +0100

    a

---
+++ src/Artist.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Artist extends Utilisateur {
+    public String nom;
+
+    public Artist() {
+    }
+
+    public Artist(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Artist nom(String nom) {
+        setNom(nom);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Artist)) {
+            return false;
+        }
+        Artist artist = (Artist) o;
+        return Objects.equals(nom, artist.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nom);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " nom='" + getNom() + "'" +
+                "}";
+    }
+
+}
