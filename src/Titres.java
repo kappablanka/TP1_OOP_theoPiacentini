cff5f3c001506e211caad2eac72da76907035705
kappablanka <theo.piacentini@gmail.com>
2022-11-04 15:31:12 +0100

    Start

---
+++ src/Titres.java
@@ -0,0 +1,12 @@
+public class Titres {
+    private int duree;
+
+    public int getDuree() {
+        return this.duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+}
